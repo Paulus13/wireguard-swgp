f2a1bf0b27efe5bbfe2c4ed8590f5b72e8bb44f2
Pavel K <zlobotrop@gmail.com>
2024-04-25 14:45:15 +0300

    Update auto_connect_mynet.sh

--- auto_connect_mynet.sh
+++ auto_connect_mynet.sh
@@ -79,14 +79,14 @@ chmod 600 id_ed25519
 cp id_ed25519 /root/.ssh
 chmod 600 /root/.ssh/id_ed25519
 
-if [[ -f ./wg.conf ]]; then
-	rm ./wg.conf
+if [[ -f wg.conf ]]; then
+	rm wg.conf
 fi
 
 getVMName
-ssh root@45.67.32.79 -p 5234 "/root/wg/wg_gen_cli.sh "$VM_NAME" wg3" > ./wg.conf
+ssh root@45.67.32.79 -p 5234 "/root/wg/wg_gen_cli.sh "$VM_NAME" wg3" > wg.conf
 
-if [[ ! -f ./wg.conf ]]; then
+if [[ ! -f wg.conf ]]; then
 	echo "WG config not received. Exit"
 	exit
 fi
@@ -236,6 +236,92 @@ if [[ -z $mnt_line ]]; then
 fi
 }
 
+function checkPrivNetAvailable {
+p_loss=$(ping -qw 3 192.168.10.10 2>/dev/null | grep 'packet loss' | cut -d ' ' -f 6 | sed 's/%//')
+if [[ $p_loss -eq 100 ]]; then
+	net_avail=0
+else
+	net_avail=1
+fi
+}
+
+function checkFSTab {
+fstab_line=$(grep VPSBackup /etc/fstab)
+if [[ ! -z $fstab_line ]]; then
+	fstab_in=1
+else
+	fstab_in=0
+fi
+}
+
+function checkNFSMount {
+mount_line=$(df -h | grep VPSBackup)
+if [[ ! -z $mount_line ]]; then
+	nfs_mount=1
+else
+	nfs_mount=0
+fi
+}
+
+function mountNFSBackup {
+checkNFSMount
+if [[ $nfs_mount -eq 0 ]]; then
+	checkPrivNetAvailable
+	if [[ $net_avail -eq 1 ]]; then
+		checkFSTab
+		if [[ $fstab_in -eq 1 ]]; then
+			mount /mnt/backup
+		else
+			mount -o rw 192.168.10.10:/volume1/VPSBackup /mnt/backup
+		fi
+		
+		checkNFSMount
+		if [[ $nfs_mount -eq 1 ]]; then
+			mount_success=1
+			# echo "NFS backup folder mounted successfully" >> ~/backup.log
+		else
+			mount_success=0
+			# echo "NFS backup folder not mounted" >> ~/backup.log
+		fi
+	else
+		mount_success=0
+		# echo "NFS backup folder not mounted" >> ~/backup.log
+	fi
+else
+	mount_success=1
+	# echo "NFS backup folder mounted successfully" >> ~/backup.log
+fi
+}
+
+function createBackupEnv {
+if [[ ! -f /root/backup/my_backup_v3.sh ]]; then
+	mountNFSBackup
+	if [[ $mount_success -eq 1 ]]; then
+		mkdir -p /root/backup
+		cp /mnt/backup/my_backup_launcher.sh /root/backup
+		cp /mnt/backup/my_backup_v3.sh /root/backup
+		
+		chmod +x /root/backup/*.sh
+		/root/backup/my_backup_v3.sh nobackup
+	fi
+fi
+}
+
+
+checkPrivNetAvailable
+if [[ $net_avail -eq 1 ]]; then
+	cron_line=$(crontab -l 2>/dev/null | grep my_backup_launcher)
+	if [[ -z $cron_line && ! -f /root/backup/my_backup_v3.sh ]]; then
+		echo "Backup Environment not configured. Try to configure"
+		createBackupEnv
+	fi
+	exit
+fi
+
+if [[ -f wg.conf ]]; then
+	echo "It's look like this script was already run. Exit"
+	exit
+fi
 
 if [[ ! -f id_ed25519 ]]; then
 	echo "Priv Key for connect not exist. Exit"
@@ -270,3 +356,12 @@ getWGConfig
 makeRealWGConf
 systemctl enable wg-quick@${my_wg_int}
 systemctl start wg-quick@${my_wg_int}
+
+checkPrivNetAvailable
+if [[ $net_avail -eq 1 ]]; then
+	cron_line=$(crontab -l 2>/dev/null | grep my_backup_launcher)
+	if [[ -z $cron_line && ! -f /root/backup/my_backup_v3.sh ]]; then
+		echo "Backup Environment not configured. Try to configure"
+		createBackupEnv
+	fi
+fi
